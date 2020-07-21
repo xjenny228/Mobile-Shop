c9fb659f8560529ee0f942378b8b57d605ba0463
xzn <1536623603@qq.com>
2020-07-21 14:10:50 +0800

    初始化了项目

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,41 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <!-- 头部，固定在顶部不动 -->
+    <div class="header">
+      <mt-header fixed title="移动商城"></mt-header>
     </div>
-    <router-view/>
+    <!-- 主体部分放置不同的组件 -->
+    <div class="main">
+      <h1>主体部分</h1>
+    </div>
+    <!-- 底部路由，用于切换不同的组件 -->
+    <div class="tabbar">
+      <nav class="mui-bar mui-bar-tab">
+        <a class="mui-tab-item mui-active" href="#tabbar">
+          <span class="mui-icon mui-icon-home"></span>
+          <span class="mui-tab-label">首页</span>
+        </a>
+        <a class="mui-tab-item" href="#tabbar-with-chat">
+          <span class="mui-icon mui-icon-email">
+            <span class="mui-badge">9</span>
+          </span>
+          <span class="mui-tab-label">消息</span>
+        </a>
+        <a class="mui-tab-item" href="#tabbar-with-contact">
+          <span class="mui-icon mui-icon-contact"></span>
+          <span class="mui-tab-label">通讯录</span>
+        </a>
+        <a class="mui-tab-item" href="#tabbar-with-map">
+          <span class="mui-icon mui-icon-gear"></span>
+          <span class="mui-tab-label">设置</span>
+        </a>
+      </nav>
+    </div>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+<style scoped>
+.header {
+  height: 40px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// 导入全局样式
+import './assets/css/global.css'
+// 导入mui
+import '../dist/css/mui.min.css'
 Vue.config.productionTip = false
+import 'mint-ui/lib/style.css'
+import { Header } from 'mint-ui';
 
+Vue.component(Header.name, Header);
 new Vue({
   router,
   render: h => h(App)
